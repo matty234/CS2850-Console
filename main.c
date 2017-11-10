6bb12c4abb656d8f8f2e06c5108bf0ef9381b966
Matthew Brown <matthew@bargrove.com>
2017-11-10 15:31:43 +0000

    MASH is a-go

--- main.c
+++ main.c
@@ -2,33 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <errno.h>
 
-void readLine(char* buf, int sz) {
+#define BUFFER_SIZE 1000
+#define MAX_ARGS 20
+#define MAX_ARG_LENGTH 100
+#define PROGRAM_NAME "mash"
+#define PROMPT "> "
+
+char* readLine( int sz) {
+    char* buf = malloc(sz * sizeof(char*));
     fgets(buf, sz, stdin);
+    size_t ln = strlen(buf) - 1;
+
+    if (*buf && buf[ln] == '\n') {
+        buf[ln] = '\0';
+    } else {
+        buf[ln+1] = '\0';
+    }
+
+    char* l = strchr(buf, 4);
+    if(l){
+        exit(EXIT_SUCCESS);
+    }
+    return buf;
+}
+
+void printHelp() {
+    printf("MASH - The Matt Shell\n\n");
+    printf("Built-Ins: cd, exit, help\n");
 }
 
 void split(char* buf, char* split[], size_t max) { //TODO use max
-    char* token = strtok(buf, " ");
+    char* token = strtok(buf, " \t\r\n\a");
     int c = 0;
     while (token != NULL)
     {
-        split[c] = (char*) malloc(100);
-        strcpy(split[c], token);
+        split[c] = (char*) malloc(max);
+        strncpy(split[c], token, strlen(token));
         c ++;
         token = strtok(NULL, " ");
     }
-    split[c+1] = "\0";
+    split[c] = 0;
 }
 
 
 void execute(char* split[]) {
+
     pid_t pid = fork();
     if(pid == 0) {
-        if(execv(split[0], split) == -1) {
-            perror(split[0]);
-            exit(0);
+        execvp(split[0], split);
+        if(errno) {
+                perror(PROGRAM_NAME);
+                exit(EXIT_FAILURE);
         }
+        exit(EXIT_SUCCESS);
     } else {
         wait(NULL);
     }
@@ -36,20 +64,24 @@ void execute(char* split[]) {
 
 
 int main(int argc, char* argv[]) {
-    int MAX_ARGS = 100;
+    int inputBufferSize = sizeof(char)*BUFFER_SIZE;
     while(&free) {
-        printf("> ");
-        char buf[100];
-        readLine(buf, 100);
+        printf(PROMPT);
 
-        char newBuf[100];
-        strncpy(newBuf, buf, strlen(buf)-1);
-        char* args[100];
+        char* buf = readLine(inputBufferSize);
+        char* args[sizeof(char*)*MAX_ARG_LENGTH*MAX_ARGS];
+        split(buf, args, MAX_ARG_LENGTH);
 
-        split(newBuf, args, 100);
         if(strncmp(args[0], "exit", 4) == 0) {
             printf("Exiting...");
             return EXIT_SUCCESS;
+        } else if(strncmp(args[0], "cd", 2) == 0) {
+            chdir(args[1]);
+            if(errno) {
+                perror(PROGRAM_NAME);
+            }
+        } else if(strncmp(args[0], "help", 4) == 0) {
+            printHelp();
         } else {
             execute(args);
         }
